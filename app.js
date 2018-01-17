75c7a074f52dfe9791a2c7ea4101bc5ef27d4601
nvtho86 <nvtho86@gmail.com>
2018-01-17 09:31:38 +0700

    up lai

---
+++ app.js
@@ -0,0 +1,42 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/shoping";
+
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   console.log("Database created!");
+//   db.close();
+// });
+
+MongoClient.connect(url, function (err, db) {
+  if (err) {
+      console.log('Unable to connect to the mongoDB server. Error:', err);
+  } 
+else 
+{
+      //HURRAY!! We are connected.
+      console.log('Connection established to', url);
+
+      // Get the documents collection
+      var collection = db.collection('restaurant');
+
+      // Find some state
+      collection.find({}).toArray
+      (
+
+          function (err, result) {
+              if (err) {
+                  console.log(err);
+              } else if (result.length) {
+        console.log(result);
+        //var result=result.length;
+
+              } else {
+                  console.log('No document(s) found with defined "find" criteria!');
+              }
+              //Close connection
+              db.close();
+          }
+
+      );
+  }
+});
